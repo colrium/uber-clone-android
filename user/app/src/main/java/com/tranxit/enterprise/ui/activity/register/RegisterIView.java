7c001f60c72d818bd1b2ee940957a0521232f9ed
Mutugi <colrium@gmail.com>
2022-01-31 09:58:33 +0300

    initial commit

---
+++ user/app/src/main/java/com/tranxit/enterprise/ui/activity/register/RegisterIView.java
@@ -0,0 +1,16 @@
+package com.tranxit.enterprise.ui.activity.register;
+
+import com.tranxit.enterprise.base.MvpView;
+import com.tranxit.enterprise.data.network.model.MyOTP;
+import com.tranxit.enterprise.data.network.model.SettingsResponse;
+import com.tranxit.enterprise.data.network.model.Token;
+
+/**
+ * Created by santhosh@appoets.com on 19-05-2018.
+ */
+public interface RegisterIView extends MvpView{
+    void onSuccess(Token token);
+    void onSuccess(MyOTP otp);
+    void onError(Throwable e);
+    void onSuccess(SettingsResponse response);
+}
